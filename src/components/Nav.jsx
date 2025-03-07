6f681f42f14803b155b153dacec094c272af19af
Ebenezer terabo <teraboebenezer@gmail.occom>
2025-03-07 19:00:13 +0000

    updated it to handle menu toggle

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,13 @@
+import React from "react"
 import { navLinks } from "../constants"
 import { headerLogo } from "../assets/images"
 import Button from "./Button"
 export default function Nav() {
+
+    const [isOpen, setIsOpen] = React.useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    } 
     return (
         <header className="px-5 py-5 max-sm:px-8 w-full">
             <nav className="flex justify-between items-center conatiner mx-auto">
@@ -10,16 +16,40 @@ export default function Nav() {
                     alt="header logo"
                     width={80} 
                 />
+                
+                {/* desktop and larger screens */}
                 <ul className="gap-4 text-[12px] text-slate-gray font-montserrat hidden md:flex">
                     {navLinks.map((link) => (
                         <li key={link.href}>
-                            <a href={link.href} className="hover:text-red-500">{link.label}</a>
+                            <a href={link.href} className="hover:text-red-500 duration-500">{link.label}</a>
                         </li>
                     ))}
                 </ul>
-                <Button label="Login" style={`text-white`}/>
-                <button className="md:hidden block text-3xl">&#9776;</button>
+                <Button label="Login" style={`text-white max-md:hidden`}/>
+                <div onClick={toggleMenu}>
+                    <span className="md:hidden block text-3xl">
+                        {
+                            isOpen ? <ion-icon name="close-circle-outline"></ion-icon> :
+                            <ion-icon name="menu-outline"></ion-icon>
+                        }
+                    </span>
+                </div>
             </nav>
+            {/* smaller screens */}
+            {
+                isOpen && (
+
+                <ul className="md:hidden font-montserrat space-y-2
+                pl-5 py-5 flex flex-col place-items-end">
+                        {navLinks.map((link) => (
+                            <li>
+                                <a href={link.href}>{link.label}</a>
+                            </li>
+                        ))}
+                        <Button label="Login" style={`text-white rounded-lg`}/>
+                </ul>
+                )
+            }
         </header>
     )
 }
